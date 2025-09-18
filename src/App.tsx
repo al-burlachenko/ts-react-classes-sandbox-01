83bb514bd38c43183747e6b6025d7d51d3839a33
al-burlachenko <alexandrburl@gmail.com>
2025-09-18 22:48:07 +0300

    bugfix, removes console logs, fix typescript

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,35 @@ import ContactsList from './components/ContactsList/ContactsList';
 import ContactFilter from './components/ContactFilter/ContactFilter';
 
 import type { stateType } from '@/utils/types';
-import { parse } from 'path';
 
 export default class App extends Component<
   { contacts: stateType[]; filter: string },
   {}
 > {
   state = {
-    contacts: [
-      { name: 'nomad1', number: '123123', key: '123' },
-      { name: 'nomad2', number: '231233', key: '2123' },
-    ],
+    contacts: [],
     filter: '',
   };
 
   componentDidMount(): void {
-    const storage: stateType = JSON.parse(localStorage.getItem('contacts'));
-    console.log('component mounted', storage);
+    const storage: stateType = JSON.parse(
+      localStorage.getItem('contacts') as string
+    );
+    // console.log('component mounted', storage);
     if (storage) this.setState({ contacts: storage });
   }
 
   componentDidUpdate(
     prevProps: Readonly<{ contacts: stateType[]; filter: string }>,
-    prevState: Readonly<{}>,
+    prevState: Readonly<{ contacts: stateType[]; filter: string }>,
     snapshot?: any
   ): void {
-    console.log('component updated', prevState);
+    // console.log('component updated', prevState);
     const prevContacts = prevState.contacts;
     const nextContacts = this.state.contacts;
     if (prevContacts !== nextContacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-      console.log('local storage write in `componentDidUpdate`');
+      // console.log('local storage write in `componentDidUpdate`');
     }
   }
 
@@ -44,19 +42,22 @@ export default class App extends Component<
   }
 
   handleSubmit = (newContact: stateType): void => {
-    if (this.state.contacts.find(contact => contact.name === newContact.name)) {
+    if (
+      this.state.contacts.find(
+        (contact: stateType) => contact.name === newContact.name
+      )
+    ) {
       alert('name already existes in the list');
     } else this.setState({ contacts: [...this.state.contacts, newContact] });
   };
 
   filterChange = (filterValue: string) => {
     this.setState({ filter: filterValue });
-    localStorage.clear();
   };
 
   filterContacts = (filterValue: string): stateType[] => {
     if (filterValue.trim()) {
-      return this.state.contacts.filter(item => {
+      return this.state.contacts.filter((item: stateType) => {
         const normalizeName = item.name.toLowerCase().trim();
         const normalizeFilter = filterValue.toLowerCase().trim();
         return normalizeName.includes(normalizeFilter);
@@ -67,7 +68,9 @@ export default class App extends Component<
 
   handleDelete = (key: string): void => {
     this.setState({
-      contacts: this.state.contacts.filter(item => item.key !== key),
+      contacts: this.state.contacts.filter(
+        (item: stateType) => item.key !== key
+      ),
     });
   };
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,15 @@
-import { Component, type ReactNode } from 'react';
+import type { ButtonHTMLAttributes, FC } from 'react';
 import css from './Button.module.css';
 
-const Button = ({
-  children,
-  onClick,
-  ...allyProps
-}: {
-  children: string;
-  onClick: () => void;
-}) => {
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+const Button: FC<Props> = ({ children, ...allyProps }) => {
   return (
-    <button className={css.button} onClick={onClick} {...allyProps}>
+    <button className={css.button} {...allyProps}>
       {children}
     </button>
   );
 };
-
 export default Button;
